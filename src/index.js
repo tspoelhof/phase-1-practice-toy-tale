ee590e6ade3435425c2ce5b976bf6a79b048e105
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-09-11 14:23:48 -0400

    toys

--- src/index.js
+++ src/index.js
@@ -13,3 +13,27 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+function createNewToy() {
+  const requestToy = {
+    method: "POST",
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "name": "Jessie",
+      "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+      "likes": 0
+    })
+  };
+  fetch("", requestToy)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+document.getElementById("create-toy-button").addEventListener("click", createNewToy);
